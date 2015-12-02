8c40e7532c3498d1a173795b698ccb98211d1210
snebel29 <nebel.sven@gmail.com>
2015-12-02 10:53:33 +0000

    add subcommand lib/subcommands/project/restoreFromFile.rb

---
+++ lib/subcommands/project/restoreFromFile.rb
@@ -0,0 +1,26 @@
+class RestoreFromFile < Subcommand
+
+  attr_reader :import_file, :subcommand_action, :subcommand_full, :description, :cmd_example
+
+  def initialize(target=nil)
+
+    @import_file = target
+    @subcommand_action = "restoreFromFile"
+    @subcommand_full = "project #{subcommand_action}"
+    @cmd_example = "#{subcommand_full} foo.zip"
+    @description = "Restore Rundeck project from a project zip file, assuming the file name match the project name, this action remove the existent project!"
+
+  end
+
+  def run
+    
+    puts "Running #{subcommand_full} #{import_file}"
+    rundeck = Rundeck.new
+
+    project_name = import_file.split('.')[0]
+    rundeck.project_delete(project_name)
+    rundeck.project_import(project_name)
+
+  end
+
+end
